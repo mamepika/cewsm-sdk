bad6ec62103a87f4ca2a780560f1345bc57919c9
TakeshiHora <a1936th@aiit.ac.jp>
2021-01-17 17:03:35 +0900

    Added AzimuthAngleTest

--- src/main/java/jp/ac/aiit/pbl/format/sep2020/AzimuthAngle.java
+++ src/main/java/jp/ac/aiit/pbl/format/sep2020/AzimuthAngle.java
@@ -2,12 +2,12 @@ package jp.ac.aiit.pbl.format.sep2020;
 
 public class AzimuthAngle {
 
-    private static final Double interval = 5.80645;
+    private static Double interval = 5.80645;
 
     private Double value;
 
     public AzimuthAngle(String intervalString){
-        this.value = interval * Long.parseLong(intervalString,2);
+        this.value = (double)Math.round(interval * Long.parseLong(intervalString,2) * 100 /100);
     }
 
     public Double value(){
