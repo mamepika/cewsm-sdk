fce20a6bcfdbaf3f9ab13f1862ab86a18322d128
mamepika <starlightprayer@gmail.com>
2021-02-05 22:07:22 +0900

    fix GeneralGuidanceToReactTest

--- src/test/java/jp/ac/aiit/pbl/format/sep2020/GeneralGuidanceToReactTest.java
+++ src/test/java/jp/ac/aiit/pbl/format/sep2020/GeneralGuidanceToReactTest.java
@@ -13,13 +13,13 @@ public class GeneralGuidanceToReactTest {
     public void inCaseOf00110000() {
         GeneralGuidanceToReact generalGuidanceToReact = GeneralGuidanceToReact.getById("00110000");
         Assert.assertThat("Turn off electrical/gas equipments and power (Japanese warning level 5)", is(generalGuidanceToReact.getInstruction()));
-        Assert.assertThat("Stay inside and Execute - Execute the <instruction>", is(generalGuidanceToReact.getResponseType()));
+        Assert.assertThat("Stay inside and Execute - Execute the instruction", is(generalGuidanceToReact.getResponseType()));
     }
         @Test
         public void inCaseOf01100011() {
             GeneralGuidanceToReact generalGuidanceToReact = GeneralGuidanceToReact.getById("01100011");
             Assert.assertThat("End of containment", is(generalGuidanceToReact.getInstruction()));
-            Assert.assertThat("AllClear – Situation is back to normal, information provided in <instruction>", is(generalGuidanceToReact.getResponseType()));
+            Assert.assertThat("AllClear – Situation is back to normal, information provided in instruction", is(generalGuidanceToReact.getResponseType()));
     }
         @Test
         public void inCaseOf01110111() {
