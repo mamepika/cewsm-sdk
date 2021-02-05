552a943695c9fbb503233e5abf1c51a9507f3780
mamepika <starlightprayer@gmail.com>
2021-02-05 14:30:17 +0900

    change version

--- src/main/java/jp/ac/aiit/pbl/format/CommonEWSMessage.java
+++ src/main/java/jp/ac/aiit/pbl/format/CommonEWSMessage.java
@@ -1,10 +1,35 @@
 package jp.ac.aiit.pbl.format;
 
 import jp.ac.aiit.pbl.format.prefix.Prefix;
+import jp.ac.aiit.pbl.format.sep2020.*;
+
+import java.time.ZonedDateTime;
 
 public interface CommonEWSMessage {
 
     public Prefix getPrefix();
+
+    public MessageType getMessageType();
+
+    public Country getCountry();
+
+    public String getProvider();
+
+    public Severity getSeverity();
+
+    public GuidanceToReact getGuidanceToReact();
+
+    public Double getLatitude();
+
+    public Double getLongitude();
+
+    public EventOnset getEventOnset();
+
+    public SemiMajorAxisLength getSemiMajorAxisLength();
+
+    public SemiMinorAxisLength getSemiMinorAxisLength();
+
+    public AzimuthAngle getAzimuthAngle();
 
     public String toString();
 
